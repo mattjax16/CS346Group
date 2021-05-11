341c1fc7afbdbfa72b6fc861d68f10195c73dbec
Jackson Prettyman <jacksonbprettyman@gmail.com>
2021-05-11 12:08:49 -0400

    more formatting changes

--- Final Project/jprett22_mbass21_wcjohn22_Final_Project_floor.m
+++ Final Project/jprett22_mbass21_wcjohn22_Final_Project_floor.m
@@ -9,51 +9,61 @@ x = 1:m;
 y = 1:n; 
 
 % creating grid
-cell_value = zeros(m,n);
-cell_value_extend = zeros(m+2,n+2);
-observation_period = {1:num_iterations};
-observation_period_extended = {1:num_iterations};
+cell_value = zeros(m, n);
+cell_value_extend = zeros(m + 2,n + 2);
+observation_period = {1:number_iterations};
+observation_period_extended = {1:number_iterations};
+
+% simulation constants
+days = 14; % number of days
+number_iterations = days; % number of iterations 
 
 % initial population values
-total_popultion = m*n; % total number of people
-Susceptible = 0; % total number of susceptible people
-Infected = 0; % total number of infected people
-Recovered = 0; % total number of recovered people
+total_popultion = m * n; % total number of people
+initial_susceptible = 0; % total number of susceptible people
+initial_infected = 0; % total number of infected people
+initial_recovered = 0; % total number of recovered people
+
+% population arrays
+susceptible_array = zeros(1, number_iterations);
+susceptible_array(1) = initial_susceptible;
+infected_array = zeros(1, number_iterations);
+infected_array(1) = initial_infected;
+recovered_array = zeros(1, number_iterations);
+recovered_array(1) = initial_recovered;
 
 % constants
-recovery_rate = .5; % rate at which infected recovers 
-transmission_constant = .50; %chance a susceptible becomes infected when ...
-                             %when interacting with 1 infected
+length_of_infection = 2; % length in days that infection lasts
+recovery_rate = 1/length_of_infection; % rate at which infected recovers 
+transmission_constant = .50; % chance a susceptible becomes infected ...
+                             % when interacting with 1 infected
                                              
 % initialization probabilities
 probability_initially_susceptible = 0.9; %probability to start susceptible 
 probability_initially_infected = 0.1; %probability to start infected
 probability_initially_phase_1 = 0.5; %probability if infected, start in phase 1
 
-%neighbor function for use in infection simulation
-%input: coordinates of grid extended
-%output odds of infection given number of infected around susceptible
-neighbor_sum = @(x,y,cell_value_extended) (1-(1-transmission_constant)^ ...
-    ((cell_value_extended(x+1,y) > 0 && cell_value_extended(x+1,y) < 3)+...
-    (cell_value_extended(x-1,y) > 0 && cell_value_extended(x-1,y) < 3)+...
-    (cell_value_extended(x,y+1) > 0 && cell_value_extended(x,y+1) < 3)+...
-    (cell_value_extended(x,y-1) > 0 && cell_value_extended(x,y-1) < 3)));
-
-
-%simulation constants
-days = 14; % number of days
-num_iterations = days; % number of iterations 
+% neighbor function for use in infection simulation
+% input: coordinates of grid extended
+% output odds of infection given number of infected around susceptible
+neighbor_sum = @(x, y, cell_value_extended) (1 - (1 - ...
+    transmission_constant)^((cell_value_extended(x + 1, y) > 0 && ...
+    cell_value_extended(x + 1, y) < 3) + (cell_value_extended(x - 1, y) ...
+    > 0 && cell_value_extended(x - 1, y) < 3) + (cell_value_extended(x, ...
+    y + 1) > 0 && cell_value_extended(x, y + 1) < 3)+ ...
+    (cell_value_extended(x, y - 1) > 0 && cell_value_extended(x ,y - 1) ...
+    < 3)));
 
 % initializing the grid and grid list
-cell_value(x,y) = cell_value(x,y) + ...
-    (rand>probability_initially_susceptible);
-cell_value_extended(2:m+1,2:n+1) = cell_value;
+cell_value(x, y) = cell_value(x, y) + (rand > ...
+    probability_initially_susceptible);
+cell_value_extended(2:m + 1, 2:n + 1) = cell_value;
 observation_period{1} = cell_value;
 observation_period_extended{1} = cell_value_extended;
 
-for i = 2:num_iterations
-cell_value = observation+period{i-1};
-cell_value_extended = observation_period_extended{i-1};
+for i = 2:number_iterations
+cell_value = observation+period{i - 1};
+cell_value_extended = observation_period_extended{i - 1};
 %Alright so this is the set up for the loop to simulate the problem i am
 %having is trying to figure out how to make sure the neighbor function only
 %applies to the 0 values. I am pretty sure this is the use of conditionals
@@ -61,12 +71,13 @@ cell_value_extended = observation_period_extended{i-1};
 %where values are 0 and use that to call on neighbor function in a loop.
 %Check the conditional i have below for checking recovery rate once we
 %have a functioning one I will remove all magic numbers
-if cell_value(x,y) == 8
-    cell_value(x,y) = 0;
+if cell_value(x, y) == 8
+    cell_value(x, y) = 0;
 end
 
-cell_value(x,y) = cell_value(x,y)+(rand>neighbor_sum(x,y,cell_value_extended));
-cell_value_extended(2:m+1,2:n+1) = cell_value;
+cell_value(x, y) = cell_value(x, y)+(rand > neighbor_sum(x, y, ...
+    cell_value_extended));
+cell_value_extended(2:m + 1,2:n + 1) = cell_value;
 observation_period{i} = cell_value;
 observation_period_extended{i} = cell_value_extended;
 end
