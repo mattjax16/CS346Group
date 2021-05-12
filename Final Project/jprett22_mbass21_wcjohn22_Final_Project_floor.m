35ca7f742ca2d2f88872936c76b97dd39e8e7f6e
wcjohn22 <59706117+wcjohn22@users.noreply.github.com>
2021-05-11 21:50:02 -0400

    Add files via upload

--- Final Project/jprett22_mbass21_wcjohn22_Final_Project_floor.m
+++ Final Project/jprett22_mbass21_wcjohn22_Final_Project_floor.m
@@ -15,12 +15,12 @@ number_iterations = days; % number of iterations
 % creating grid
 cell_value = zeros(rows, cols);
 cell_value_extend = zeros(rows + 2, cols + 2);
-observation_period = {1:number_iterations};
-observation_period_extend = {1:number_iterations};
+observation_period = {};
+observation_period_extend = {};
 
 % constants
 length_of_infection = 2; % length in days that infection lasts
-length_of_immunity = 5
+length_of_immunity = 5;
 recovery_rate = 1 / length_of_infection; % rate at which infected recovers 
 transmission_constant = .25; % chance a susceptible becomes infected ...
                              % when interacting with 1 infected
@@ -67,24 +67,27 @@ infected_array = zeros(1, number_iterations);
 infected_array(1) = initial_infected;
 recovered_array = zeros(1, number_iterations);
 
-for i = 2:number_iterations
-    cell_value = observation + period{i - 1};
-    cell_value_extend = observation_period_extend{i - 1};
-    %Alright so this is the set up for the loop to simulate the problem i am
-    %having is trying to figure out how to make sure the neighbor function only
-    %applies to the 0 values. I am pretty sure this is the use of conditionals
-    %but am not sure and not confident. One thought would be to find all index
-    %where values are 0 and use that to call on neighbor function in a loop.
-    %Check the conditional i have below for checking recovery rate once we
-    %have a functioning one I will remove all magic numbers
-    if cell_value(x, y) == length_of_infection + length_of_immunity + 1 
-        cell_value(x, y) = 0;
-    end
-
-    cell_value(x, y) = cell_value(x, y)+(rand > neighbor_sum(x, y, ...
-                       cell_value_extend));
-    cell_value_extend(2:rows + 1,2:cols + 1) = cell_value;
-    observation_period{i} = cell_value;
-    observation_period_extend{i} = cell_value_extend;
+for i = 2:number_iterations    
+     cell_value = observation_period{i - 1};
+     cell_value_extend = observation_period_extend{i - 1};
+     for row = 2:rows+1
+         for col = 2:cols+1
+             if cell_value(row-1,col-1) == 0
+                cell_value(row-1,col-1) = cell_value(row-1,col-1) + ... 
+                (rand < probability_of_infection(row,col,cell_value_extend));
+             elseif cell_value(row-1,col-1) > 0 && cell_value(row-1,col-1) < 8
+                cell_value(row-1,col-1) = cell_value(row-1,col-1)+1;
+                
+                if cell_value(row-1,col-1) == 8
+                    cell_value(row-1,col-1) = 0;
+                 
+                end
+             end
+             
+         end 
+     end
+     cell_value_extend(2:rows + 1, 2:cols + 1) = cell_value;
+     observation_period{i} = cell_value;
+     observation_period_extend{i} = cell_value_extend;
+     
 end
-
